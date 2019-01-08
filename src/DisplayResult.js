6df12b3b7fdfc887d7612aa8d41789f5ae2a2df2
johannaanden <johannaanden@hotmail.com>
2019-01-08 10:48:26 +0100

    Creates constructor and displays message with setState

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { bmiCalculation } from './BMICalculator'
 
 class DisplayResult extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bmiMessage: ''
+        };
+    }
+
     calculate() {
         var weight= this.props.weight;
         var height= this.props.height;
-
-        return bmiCalculation(weight, height);
+        this.setState({ bmiMessage: bmiCalculation(weight, height) });
     }
 
     render() {
         return (
             <div>
                 <button onClick={() => this.calculate()}Calculate></button>
+                <div>{this.state.bmiMessage}</div>
             </div>
         )
     }
