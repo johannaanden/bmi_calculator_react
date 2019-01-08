7282538756629b5bf30a9b1875b7bff401c3cc9c
johannaanden <johannaanden@hotmail.com>
2019-01-08 16:07:04 +0100

    Configurates setup to be able to run acceptance tests properly

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,19 @@
+const port = process.env.TEST_SERVER_PORT
+    ? Number(process.env.TEST_SERVER_PORT)
+    : 3000
+
+process.env.TEST_SERVER_PORT = port
+
+module.exports = {
+    launch: {
+        headless: process.env.CI === 'true',
+        slowMo: 50,
+        devtools: true
+    },
+    browserContext: process.env.INCOGNITO ? 'incognito' : 'default',
+    server: {
+        command: `PORT=${port} react-scripts start`,
+        port,
+        launchTimeout: 4000,
+    },
+} 
