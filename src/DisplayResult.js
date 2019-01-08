37cb4d298e5c17f93b81843826fa21aa0af5c63a
johannaanden <johannaanden@hotmail.com>
2019-01-08 07:16:35 +0100

    Creates DisplayResult component

---
+++ src/DisplayResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DisplayResult extends Comment {
+
+    render() {
+        return (
+            <div id='response'>
+            </div>
+        )
+    }
+}
+
+export default DisplayResult
