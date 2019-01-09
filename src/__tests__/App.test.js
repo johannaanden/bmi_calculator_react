dce2b9846ae6ddf1230da2c2377dcac9421a36b0
johannaanden <johannaanden@hotmail.com>
2019-01-09 15:07:20 +0100

    Creates Component testing for App.js - Test 'Starts with metric as the standard calculator' going green

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { stub } from 'sinon';
+
+import App from '../App';
+
+describe ('<App />', () => {
+
+    it('Starts with metric as the standard calculator', () => {
+        const component = shallow(<App />);
+        const weightLabel = <label>Weight (kg)</label>;
+        const heightLabel = <label>Height (cm)</label>;
+        expect(component.contains(weightLabel)).toEqual(true);
+        expect(component.contains(heightLabel)).toEqual(true);
+    })
+
+})
